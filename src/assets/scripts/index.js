04bdfe9e342bee2b1a17957da8459d9dbb7d6d92
Felix Wrba <felix.wrba@web.de>
2026-08-16 14:56:01 +0200

    Added css components

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,4 +1,16 @@
 import { $ } from "./lib";
 
-const btn = $('#btn');
-btn.addEventListener('click', () => btn.innerText = Number(btn.innerText) + 1);
+const btnIncrease = $('#btn-increase');
+const btnReset = $('#btn-reset');
+
+let count = 0;
+
+btnIncrease.addEventListener('click', () => {
+    count++;
+    btnIncrease.innerText = count;
+});
+
+btnReset.addEventListener('click', () => {
+    count = 0;
+    btnIncrease.innerText = '0';
+});
